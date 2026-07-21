45548636cd8c4f3535c1eac0d2874ac4a0ab49f5
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-07-21 10:43:06 +0700

    Menambahkan friend class antara orang dan siswa

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -2,3 +2,15 @@
 #include <string>
 using namespace std;
 
+class siswa; // forward declaration
+
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa; // siswa boleh akses private orang
+};
+
